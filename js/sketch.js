b5421941b27a258f4c81246d34e2cfc07e9a23c5
Josue Huaman <64330912+Josuehu@users.noreply.github.com>
2022-09-09 17:29:23 -0500

    var puntos

--- js/sketch.js
+++ js/sketch.js
@@ -30,7 +30,7 @@ function setup() {
     }
 
     var data = [];
-    var total_puntos = 25
+    var total_puntos = 5
     for (let i = 0; i < total_puntos; i++) {
         var x = Math.floor(Math.random() * height);
         var y = Math.floor(Math.random() * height);
